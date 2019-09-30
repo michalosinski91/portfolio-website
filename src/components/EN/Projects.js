9a8ed392097b2a15ae5b8c1e39cee9c1bb44903d
michalosinski91 <mosinski@hotmail.co.uk>
2019-09-30 17:26:42 +0100

    Project section content added

--- src/components/EN/Projects.js
+++ src/components/EN/Projects.js
@@ -14,16 +14,33 @@ const Projects = () => {
                     <Item.Image src={logo} size='large' />
                     <Item.Content>
                         <Item.Header>Animal Shelter web app</Item.Header>
-                        <Item.Meta>Environment: JavaScript, React, Apollo, GraphQL, MongoDB, GoogleMaps API, JWT, SemanticUI</Item.Meta>
-                        <Item.Description>
-                            lorem ipsum
-                            lorem ipsum
+                        <Item.Meta>Environment: JavaScript, React, Apollo, GraphQL, MongoDB, Mongoose, GoogleMaps API, JWT, SemanticUI</Item.Meta>
+                        <Item.Description style={{ marginTop: '15px'}}>
+                            This application allows the user to view a map of Poland with pinponted location of nearly 100 real animal shelters. 
+                            The user can select a shelter, either from the map or a list, to view that shelter's detailed page.
+                            The shelter's information include real contact details (address, phone number, website) and a list of animals currently at the shelter (mock data).
+                            The user can select an animal to view more details about that animal.
+                            There is also a message-box, which allows the user to send an enquiry to that shelter.
+                            The user can create an account on the website, either as an individual user or as an admin for a shelter.
+                            If assiged to a shelter, the admin can then alter the details of the shelter (or delete it entirely), and can amend the details of the animals at that shelter,
+                            including adding and removing animals from the database, or altering their details.
+                            Registered users also have access to an inbox, which allows them to view and respond to messages received.
+                        </Item.Description>
+                        <Item.Description style={{ marginTop: '15px'}}>
+                            The application was built using JavaScript (ES6), React (including Hooks) and Apollo on the frontend, with SemanticUI for styling. The map interface is created using GoogleMaps API.
+                            The server runs in Node.js environment, utilises ApolloServer. GraphQL is used to build the API, MongoDB Atlas used for cloud database, with Mongoose package used to create schemas.
+                            Authentication/Authorization is using JSON Web Tokens (JWT). The data of the shelters (name, location, website, contact number) was web-scraped using Puppeteer.js. Images are uploaded and hosted using Cloudinary service.
+                        </Item.Description>
+                        <Item.Description style={{ marginTop: '15px'}}>
+                            I am currently working on this application, and the planned changes/additions for the near future include 
+                            social media feed (e.g. Twitter) for individual shelters if that particular shelter has an active a social media account on the selected platform, making a donation to a shelter, 
+                            and improvement to the authorization/authentication flow (similar to the one I have implemented in the 'JWT Node' repo described below).
                         </Item.Description>
                         <Item.Extra>
-                            <Button floated='left' color='green' style={{ marginTop: '15px', minWidth: '150px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
-                                <Icon style={{ marginBottom: 8 }} size='large' name='globe' /> Open the app
+                            <Button floated='right' color='green' style={{ marginTop: '15px', minWidth: '150px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
+                                <Icon style={{ marginBottom: 8 }} size='large' name='globe' /> Open the app (Polish language only)
                             </Button>
-                            <Button floated='center' color='black' style={{ marginTop: '15px', minWidth: '150px', fontSize: '14px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
+                            <Button floated='right' color='black' style={{ marginTop: '15px', minWidth: '150px', fontSize: '14px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
                                 <Icon style={{ marginBottom: 8 }} size='large' name='github square' inverted /> See the code
                             </Button>
                         </Item.Extra>
--- src/components/PL/Projekty.js
+++ src/components/PL/Projekty.js
@@ -13,17 +13,37 @@ const Projekty = () => {
                 <Item>
                     <Item.Image src={logo} size='large' />
                     <Item.Content>
-                        <Item.Header>Schroniska dla zwierząt</Item.Header>
-                        <Item.Meta>Użyte technologie: JavaScript, React, Apollo, GraphQL, MongoDB, GoogleMaps API, JWT, SemanticUI</Item.Meta>
-                        <Item.Description>
-                            lorem ipsum
-                            lorem ipsum
+                        <Item.Header>Portal schronisk dla zwierząt</Item.Header>
+                        <Item.Meta>Użyte technologie: JavaScript, React, Apollo, GraphQL, MongoDB, Mongoose, GoogleMaps API, JWT, SemanticUI</Item.Meta>
+                        <Item.Description style={{ marginTop: '15px'}}>
+                            W tej aplikacji użytkownik może zobaczyć mapę Polski z zaznaczonm położeniem prawie 100 prawdziwych schronisk dla zwierząt.
+                            Użytkownim może wybrać każde ze schronisk poprzez mapę lub z listy, by zobaczyć szczegóły wybranej placówki.
+                            Znajdują się tam prawdziwe dane kontaktowe schroniska (adres pocztowy, number kontaktowy, link do strony internetowej) i lista zwierząt obecnie znajdujących się w tej placówce.
+                            Użytkownik może wybrać zwierzę z listy by wyświetlić szczegółowe dane wybranego zwierzęcia.
+                            Jest także możliwość wysłania wiadomości do schroniska.
+                            Użytkownik może zarejestrować konto na portalu dla użytku własnego lub w roli administratora dla jednego ze schronisk.
+                            Jeżeli do użytkownika przypisana jest rola administratora, może on zmieniać szczegóły kontaktowe placówki (albo usunąć ją całkowicie), 
+                            a także uaktualniać bazę danych zwierząt - zwierzęta mogą być dodane i usunięte, lub szczegóły istniejących zwierząt mogą być zmienione.
+                            Zalogowani użytkownicy mają też dostęp do skrzynki pocztowej, gdzie mogą wyświetlić i odpowiedzieć na wiadomości.
+                        </Item.Description>
+                        <Item.Description style={{ marginTop: '15px'}}>
+                            Ta aplikacja została napisana w języku JavaScript (ES6), z użyciem React (z Hookami) i Apollo na frontendzie, a do stworzenia interfejsu użytkownika wykorzystałem bibliotekę SemanticUI.
+                            Mapa na stronie głównej wykorzystuje API GoogleMaps.
+                            Serwer jest napisany w środowisku Node.js i wykorzystuje ApolloServer. API napisane jest w standardzie GraphQL i łączy się z bazą danych MongoDB Atlas (chmura) przy użyciu biblioteki Mongoose. 
+                            Autoryzacja wykorzystuje standard JSON Web Tokens (JWT).
+                            Dane schronisk (nazwa, lokalizacja, strona internetowa, number kontaktowy) zostały zebrane poprzez web-scraping z biblioteką Puppeteer.js. 
+                            Zdjęcia przechowywane są na serwisie Cloudinary.
+                        </Item.Description>
+                        <Item.Description style={{ marginTop: '15px'}}>
+                            Praca nad tą aplikacją nie jest jeszcze skończona i w najbliższym czasie planuję dodać, na przykład, 
+                            wtyczkę z wiadomościami z portali społecznościowch (np. Twitter) dla poszczególnych schronisk jeśli schronisko posiada aktywne konta w wybranym portalu,
+                            możliwość przekazania darowizny dla poszczególnych placówek, a także ulepszyć metodę autoryzacji w aplikacji (na wzór tej udostępnionej w niżej udostępnionym projekcie 'JWT Node').
                         </Item.Description>
                         <Item.Extra>
-                            <Button floated='left' color='green' style={{ marginTop: '15px', minWidth: '150px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
+                            <Button floated='right' color='green' style={{ marginTop: '15px', minWidth: '150px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
                                 <Icon style={{ marginBottom: 8 }} size='large' name='globe' /> Otwórz aplikację
                             </Button>
-                            <Button floated='center' color='black' style={{ marginTop: '15px', minWidth: '150px', fontSize: '14px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
+                            <Button floated='right' color='black' style={{ marginTop: '15px', minWidth: '150px', fontSize: '14px' }} as='a' href='https://www.linkedin.com/in/michalosinski91' target='blank'>
                                 <Icon style={{ marginBottom: 8 }} size='large' name='github square' inverted /> Zobacz kod aplikacji
                             </Button>
                         </Item.Extra>
